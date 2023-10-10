8e8219fbe06b0e175f06bbe095379f0377e9e06b
Mukul <tilak.mukul@hotmail.com>
2023-10-10 06:21:50 +0530

    end of crud auction service

--- src/AuctionService/Data/AuctionDbContext.cs
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Data
 {
-    class AuctionDbContext: DbContext
+    public class AuctionDbContext: DbContext
     {
         public AuctionDbContext(DbContextOptions options) : base (options)
         {
